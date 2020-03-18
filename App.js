407f1a62aee44ad447e9b2757ae199fbfd08883a
Andrew Aitken <andy-97@live.co.uk>
2020-03-18 13:49:39 +0000

    Nav Stack

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { startAsync } from 'expo/build/AR';
+
 
 function HomeScreen({navigation}) {
   return (
@@ -16,15 +19,39 @@ function HomeScreen({navigation}) {
   );
 }
 
-function New_Story_Details() {
+
+const Stack = createStackNavigator();
+
+function New_Story_Details({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text> 
+      <Text>Story Details Screen</Text> 
+      <View>
+      <Story_Stack.Navigator>
+        <Story_Stack.Screen name="Story_Creation" 
+        component={Story_Creation} />
+      </Story_Stack.Navigator>
+        <Button
+          title="Go to Story Creation"
+          onPress={() => navigation.navigate('Story_Creation')}
+        />     
+        </View>
+    </View> 
+
+  );
+}
+
+const Story_Stack = createStackNavigator();
+
+
+function Story_Creation() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Story Creation Screen</Text> 
     </View>
   );
 }
-const Stack = createStackNavigator();
- 
+
 function App() {
   return (
     <NavigationContainer>
