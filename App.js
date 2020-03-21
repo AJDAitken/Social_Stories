d61fdbc1e16b667c8a2c821bbd685a7aec528147
Andrew Aitken <andy-97@live.co.uk>
2020-03-21 13:17:15 +0000

    Basic Nav Finished, aadding images and building sotry creation frame in progress

--- App.js
+++ App.js
@@ -20,20 +20,27 @@ function HomeScreen({navigation}) {
 }
 
 
+
+function Story_Creation() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Story Creation Screen</Text> 
+      <Image source={require('./assets/Images/School/Backpack.svg')} />
+    </View>
+  );
+} 
+
 const Stack = createStackNavigator();
 
 function New_Story_Details({navigation}) {
   return (
+
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Story Details Screen</Text> 
       <View>
-      <Story_Stack.Navigator>
-        <Story_Stack.Screen name="Story_Creation" 
-        component={Story_Creation} />
-      </Story_Stack.Navigator>
         <Button
           title="Go to Story Creation"
-          onPress={() => navigation.navigate('Story_Creation')}
+          onPress={() => navigation.navigate('Story Creation')}
         />     
         </View>
     </View> 
@@ -44,14 +51,6 @@ function New_Story_Details({navigation}) {
 const Story_Stack = createStackNavigator();
 
 
-function Story_Creation() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Story Creation Screen</Text> 
-    </View>
-  );
-}
-
 function App() {
   return (
     <NavigationContainer>
@@ -60,6 +59,8 @@ function App() {
         component={HomeScreen} 
         options={{ title: 'Main Menu'}}/>
         <Stack.Screen name="Details" component={New_Story_Details} />
+        <Stack.Screen name="Story Creation" 
+          component={Story_Creation} />
       </Stack.Navigator>
     </NavigationContainer>
   );
