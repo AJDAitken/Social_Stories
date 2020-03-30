a99174f92e1410b9243e53f631c56b78a3efe7d2
Andrew Aitken <andy-97@live.co.uk>
2020-03-30 16:01:35 +0100

    Added scrollview and images to story creation

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, ScrollView, style, Animated, PanResponder} from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { startAsync } from 'expo/build/AR';
+import {Image} from 'react-native' ;
+import { render } from 'react-dom';
+
+
 
 
 function HomeScreen({navigation}) {
@@ -23,10 +25,16 @@ function HomeScreen({navigation}) {
 
 function Story_Creation() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Story Creation Screen</Text> 
-      <Image source={require('./assets/Images/School/Backpack.svg')} />
-    </View>
+    <View style={styles.bottomView}>
+      <ScrollView style={styles.scrollViewStyle} horizontal={true}>
+      <Image source={require('./assets/Images/School/backpack.png')} style={styles.imageStyle}  />
+      <Image source={require('./assets/Images/School/basketball.png')} style={styles.imageStyle}  />
+      <Image source={require('./assets/Images/School/book.png')} style={styles.imageStyle}  />
+      <Image source={require('./assets/Images/School/bookshelf.png')} style={styles.imageStyle}  />
+      <Image source={require('./assets/Images/School/desktop.png')} style={styles.imageStyle}  />          
+      </ScrollView>
+    </View>   
+
   );
 } 
 
@@ -64,8 +72,50 @@ function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
+  
 }
+const styles = StyleSheet.create(
+  {
+      MainContainer:
+      {
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingTop: ( Platform.OS === 'ios' ) ? 20 : 0
+      },
+   
+      bottomView:{
+   
+        width: '100%', 
+        height: 175, 
+        backgroundColor: '#808080',
+         
+        alignItems: 'flex-start',       
+        position: 'absolute',
+        bottom: 0
+      },
+
+      imageStyle:{
+        width: 150, 
+        height: 150,
+        justifyContent: 'space-around',
+        flex:1,
+        flexDirection: "row",
+      },
+
+      scrollViewStyle:{
+        marginHorizontal: 20,
+        marginVertical: 20
+        
+      },
+
+      textStyle:{
+   
+        color: '#fff',
+        fontSize:22
+      }
+
+  });
 
 export default App;
  
-
